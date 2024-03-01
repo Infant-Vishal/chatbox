bcb7870011df465d3958714e9bd68d6f4a3f68e3
Infant Vishal <infantvishal28@gmail.com>
2024-03-01 06:23:01 +0530

    added grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import Chat from "./components/Chat";
+import Contacts from "./components/Contacts";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      ChatBox
+    <div className="main-container">
+      <Contacts />
+      <Chat />
+      <Profile />
     </div>
   );
 }
